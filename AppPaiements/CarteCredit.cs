21aa16e0deed45f11170ba81e87e1d70b87bf952
Kadji Maxime <300141650@monboreal.ca>
2024-10-23 15:26:11 -0400

    Ajout de la classe fille CarteCredit

---
+++ AppPaiements/CarteCredit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class CarteCredit : Paiement
+    {
+        public int NumeroCarte { get; set; }
+
+        public CarteCredit(double montant, string description, int numeroCarte)
+            : base(montant, description)
+        {
+            NumeroCarte = numeroCarte;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Paiement par Carte de Crédit : {Montant}$, {Description}, Carte : {NumeroCarte}");
+        }
+    }
+}
--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using AppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit achat1 = new CarteCredit(50.0, "Achat de livre", 123456789);
+        CarteCredit achat2 = new CarteCredit(150.0, "Achat de vêtements", 987654321);
+        CarteCredit achat3 = new CarteCredit(300.0, "Achat de téléphone", 586472159);
+
+        achat1.AfficherDetails();
+        achat2.AfficherDetails();
+        achat3.AfficherDetails();
     }
 }
